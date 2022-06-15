a2b557fe522f2a257ee172e6e79e36ca4848e904
wml201314 <1134122579@qq.com>
2022-06-15 09:21:54 +0800

    字体修改保存

--- src/utils/setting.js
+++ src/utils/setting.js
@@ -12,12 +12,12 @@
   function handleFontSize() {
     // 设置网页字体为默认大小
     WeixinJSBridge.invoke('setFontSizeCallback', {
-      fontSize: 1
+      fontSize: 0
     })
     // 重写设置网页字体大小的事件
     WeixinJSBridge.on('menu:setfont', function () {
       WeixinJSBridge.invoke('setFontSizeCallback', {
-        fontSize: 1
+        fontSize: 0
       })
     })
   }
